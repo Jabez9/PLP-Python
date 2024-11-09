af888169e7fd4f38e25103c3cc23a97a8bf600ed
Jabez9 <mukoshijabez@gmail.com>
2024-11-09 14:06:56 +0300

    Assignment one committed

---
+++ Assignment1/user_input.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#asking for the user's name
+name = input("Enter your name: ")
+
+#asking for the user's age
+age = input("Enter your age: ")
+
+#asking for the user's location
+location = input("Enter your location: ")
+
+#output
+print(f"Hello {name} , you are {age} years old and you live in {location}.")
